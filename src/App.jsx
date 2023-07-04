35c5028c9d92bc6d5d6bfb3ab5e2c7f2ccfd6b33
MichelaCerma <michela.cerma02@gmail.com>
2023-07-04 17:58:43 +0200

    feat: column section

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,75 @@ function App() {
           esse cum?
         </p>
       </div>
+      <div className="wrapper">
+        <div className="row">
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+        </div>
+        <div className="row">
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+        </div>
+        <div className="row">
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+        </div>
+        <div className="row">
+          <div className="col-4">col4</div>
+          <div className="col-4">col4</div>
+          <div className="col-4">col4</div>
+        </div>
+        <div className="row">
+          <div className="col-5">col5</div>
+          <div className="col-7">col7</div>
+        </div>
+        <div className="row">
+          <div className="col-12">col12</div>
+        </div>
+        <div className="row">
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+          <div className="col-6">col6</div>
+        </div>
+        <div className="row space-between">
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+        </div>
+        <div className="row">
+          <div className="col-4">col4</div>
+          <div className="col-8">col8</div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12 col-sm-6 col-md-3">
+          <img src="https://picsum.photos/200/200" />
+        </div>
+        <div className="col-xs-12 col-sm-6 col-md-3">
+          <img src="https://picsum.photos/200/200" />
+        </div>
+        <div className="col-xs-12 col-sm-6 col-md-3">
+          <img src="https://picsum.photos/200/200"  />
+        </div>
+        <div className="col-xs-12 col-sm-6 col-md-3">
+          <img src="https://picsum.photos/200/200"  />
+        </div>
+      </div>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,11 @@ export default defineConfig({
         @import "./src/scss/variables/colors.scss";
         @import "./src/scss/variables/variables.scss";
         @import "./src/scss/variables/sizes.scss";
+        @import "./src/scss/variables/typography.scss";
         @import "./src/scss/helpers/mixins.scss";
         @import "./src/scss/helpers/devices.scss";
         @import "./src/scss/helpers/functions.scss";
+        @import "./src/scss/layouts/column.scss";
         
 
   `,
